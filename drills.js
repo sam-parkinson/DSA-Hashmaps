561b3679e45102b9f22001adaa2e436af8b36888
Sam Parkinson <parkinsonsp42@gmail.com>
2019-08-26 21:57:07 -0400

    Continue working drills

--- drills.js
+++ drills.js
@@ -1,1 +1,29 @@
 const HashMap = require('./hashMap');
+
+function main() {
+  const arr = [
+    {"Hobbit": "Bilbo"}, 
+    {"Hobbit": "Frodo"},
+    {"Wizard": "Gandolf"}, 
+    {"Human": "Aragon"}, 
+    {"Elf": "Legolas"}, 
+    {"Maiar": "The Necromancer"},
+    {"Maiar": "Sauron"}, 
+    {"RingBearer": "Gollum"}, 
+    {"LadyOfLight": "Galadriel"}, 
+    {"HalfElven": "Arwen"},
+    {"Ent": "Treebeard"},
+  ];
+  let lor = new HashMap();
+  lor.MAX_LOAD_RATIO = 0.5;
+  lor.SIZE_RATIO = 3;
+  arr.forEach(item => {
+    const type = Object.keys(item)[0];
+    lor.set(type, item[type]);
+  })
+  return lor;
+}
+
+module.exports = {
+  main,
+}
--- drills.md
+++ drills.md
@@ -1,4 +1,11 @@
 # Hash Map Drills
 
+## Drill 1
+
+No, values for the Necromancer and Bilbo were not hashed
+Length is shorter than it should be -- array needs to be resized
+
+Capacity is 8, which is the index of the last value in the array
+
 ## Drill 2
 
